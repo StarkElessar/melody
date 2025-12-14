eef2d17f56f8a48666fdce54edabeb2641c663cb
stark <serj.elessar@gmail.com>
2025-12-14 23:54:18 +0300

    refactor(ui): simplify event listener logic and improve styles for disabled state

--- src/js/init-active-link.js
+++ src/js/init-active-link.js
@@ -1,20 +1,18 @@
 export const initActiveLink = () => {
-	document.addEventListener('DOMContentLoaded', () => {
-		const navLinks = document.querySelectorAll('.navbar__list-link'),
-			navMenu = document.querySelector('.navbar__list'),
+	const navLinks = document.querySelectorAll('.navbar__list-link');
+	const navMenu = document.querySelector('.navbar__list');
 
-			removeActiveClass = (item) => item.classList.remove('navbar__list-link--active'),
-			addActiveClass = (item) => item.classList.add('navbar__list-link--active');
+	const removeActiveClass = (item) => item.classList.remove('navbar__list-link--active');
+	const addActiveClass = (item) => item.classList.add('navbar__list-link--active');
 
-		navMenu.addEventListener('click', (event) => {
-			const target = event.target;
+	navMenu.addEventListener('click', (event) => {
+		const target = event.target;
 
-			if (target && target.classList.contains('navbar__list-link')) {
-				if (!target.classList.contains('navbar__list-link--active')) {
-					navLinks.forEach((link) => removeActiveClass(link));
-					addActiveClass(target);
-				}
+		if (target && target.classList.contains('navbar__list-link')) {
+			if (!target.classList.contains('navbar__list-link--active')) {
+				navLinks.forEach((link) => removeActiveClass(link));
+				addActiveClass(target);
 			}
-		});
+		}
 	});
-}
+};
